498967ea9c6f055f29fb24041520789acfa60497
unknown <evikody06@gmail.com>
2024-10-15 01:09:59 +0800

    Use if statements to find the highest number

--- highest_number_finder/highest_num_finder.py
+++ highest_number_finder/highest_num_finder.py
@@ -9,5 +9,16 @@ number5 = float(input("Please enter the fifth number: "))
 highest_num = number1 # Set highest_num to number1
 
 # Use if statements to find the highest number
+if number2 > highest_num: # If number2 is greater than highest_num
+    highest_num = number2 # Update highest_num to number2
+
+if number3 > highest_num: # If number3 is greater than highest_num
+    highest_num = number3 # Update highest_num to number3
+
+if number4 > highest_num: # If number4 is greater than highest_num
+    highest_num = number4 # Update highest_num to number4
+
+if number5 > highest_num: # If number5 is greater than highest_num
+    highest_num = number5 # Update highest_num to number5
 
 # Print the highest number
