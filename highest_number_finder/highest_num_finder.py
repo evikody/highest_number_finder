9a7386b3513d577a784fc7a5a5d6fda079ffc8fa
unknown <evikody06@gmail.com>
2024-10-15 00:58:00 +0800

    Ask the user to input 5 numbers separately

--- highest_number_finder/highest_num_finder.py
+++ highest_number_finder/highest_num_finder.py
@@ -1,4 +1,9 @@
 # Ask the user to input 5 numbers separately
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+number3 = float(input("Please enter the third number: "))
+number4 = float(input("Please enter the fourth number: "))
+number5 = float(input("Please enter the fifth number: "))
 
 # Set a variable as the highest number
 
